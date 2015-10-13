7309120d33df5827f18f429efcc434a9c871c494
Marc Thomas <marct@lostmy.name>
2015-10-13 16:57:41 +0100

    Add colors, and button on homepage

---
+++ src/js/components/button.jsx
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import React from 'react';
+import { Link } from 'react-router';
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleOnClick() {
+    this.props.onClick();
+  }
+
+  render () {
+    return (
+      <button className="button" onClick={this.handleOnClick.bind(this}>
+        {this.props.chilren}
+      </button>
+    );
+  }
+}
+
+export default Button;
--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import ItemList from './item-list.jsx';
+import Button from './button.jsx';
 import AddItem from './add-item.jsx';
 import { requireAuth } from '../app/functions.jsx';
 import auth from './auth.jsx';
@@ -10,7 +11,8 @@ var Home = requireAuth(class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      showForm: false
     };
   }
 
@@ -64,6 +66,12 @@ var Home = requireAuth(class extends React.Component {
     });
   }
 
+  handleButtonClick() {
+    this.setState({
+      showForm: true
+    });
+  }
+
   handleOnBoughtStatusChange(id, isBought, index) {
     $.ajax({
       url: `${ITEMS_URL}/${id}`,
@@ -87,9 +95,10 @@ var Home = requireAuth(class extends React.Component {
   render() {
     var token = auth.getToken();
     return (
-      <div className="xmas-list">
+      <div className="xmas-list" + {this.state.showForm ? " js-show-add-form" : ""}>
         <ItemList items={this.state.items} onBoughtStatusChange={this.handleOnBoughtStatusChange.bind(this)}>
         </ItemList>
+        <Button onClick={this.handleButtonClick.bind(this)}>Add New Item</Button>
         <AddItem onFormSubmit={this.handleFormSubmit.bind(this)} />
       </div>
     );
