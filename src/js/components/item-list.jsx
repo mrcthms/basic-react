104145338f80495d5186f687ec0eec02481ae0be
Marc Thomas <marct@lostmy.name>
2015-11-14 18:28:09 +0000

    Filter already bought items into own tab

--- src/js/components/item-list.jsx
+++ src/js/components/item-list.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import LineItem from './line-item.jsx';
+import classNames from 'classNames';
 
 class ItemList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isBought: false
+    };
   }
 
   handleOnBoughtStatusChange(id, isBought, index) {
@@ -14,11 +18,17 @@ class ItemList extends React.Component {
     this.props.onDeleteClick(id, index);
   }
 
+  handleTabClick(bool) {
+    this.setState({
+      isBought: bool
+    });
+  }
+
   render() {
     var itemsList = this.props.items.map((item, index) => {
       var whoFor = item.whoFor.toLowerCase();
       var whoIsBuying = item.whoIsBuying.toLowerCase();
-      if (whoFor.indexOf(this.props.filterData.whoFor.toLowerCase()) > -1 && whoIsBuying.indexOf(this.props.filterData.whoIsBuying.toLowerCase()) > -1) {
+      if (whoFor.indexOf(this.props.filterData.whoFor.toLowerCase()) > -1 && whoIsBuying.indexOf(this.props.filterData.whoIsBuying.toLowerCase()) > -1 && item.isBought === this.state.isBought) {
         return (
           <LineItem {...item} key={ index } index={index} onBoughtStatusChange={this.handleOnBoughtStatusChange.bind(this)} onDeleteClick={this.handleDeleteClick.bind(this)} />
         );
@@ -28,10 +38,14 @@ class ItemList extends React.Component {
     return (
       <div className='item-list [ grid ]'>
         <div className='[ grid__col grid__col--half ]'>
-          <h1 className='item-list__title'>Your Items To Buy</h1>
+          <h1 className='item-list__title'>
+            <a href='javascript:void(0)' className={classNames('item-list__title-link', { 'item-list__title-link--active': this.state.isBought === false })} onClick={this.handleTabClick.bind(this, false)}>Your Items To Buy</a>
+          </h1>
         </div>
         <div className='[ grid__col grid__col--half ]'>
-          <h1 className='item-list__title'>Items You Have Bought</h1>
+          <h1 className='item-list__title'>
+            <a href='javascript:void(0)' className={classNames('item-list__title-link', { 'item-list__title-link--active': this.state.isBought === true })} onClick={this.handleTabClick.bind(this, true)}>Items You Have Bought</a>
+          </h1>
         </div>
         {itemsList}
       </div>
